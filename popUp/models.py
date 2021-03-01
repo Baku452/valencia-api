dbfbaabf12d77e1663ce1030531a21844a997430
anaid jimenez <info@valenciatravelcusco.com>
2021-02-28 23:41:03 -0500

    fix popup image size

--- popUp/models.py
+++ popUp/models.py
@@ -12,7 +12,7 @@ class PopUp(models.Model):
     image = models.FileField(upload_to='images/popup/', blank=True, null=True)
     original = ImageSpecField(
         source='image',
-        processors=[ResizeToFill(432, 342)],
+        processors=[ResizeToFill(346, 718)],
         format='JPEG',
         options={'quality': 98},
     )
