d575dc6feebf0ea3d6bea8f1de64304849b8d7f8
Aldair Huamani <aiacos12@outlook.com>
2021-06-11 13:06:24 -0500

    adding pagination django

--- valenciaApi/settings.py
+++ valenciaApi/settings.py
@@ -108,6 +108,8 @@ CKEDITOR_CONFIGS = {
        },
    }
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
