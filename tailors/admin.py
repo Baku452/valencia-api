98d1ddc43b0c6c291ef5638338bc160d90cef3d5
Aldair Huamani Cusi <32407895+Baku452@users.noreply.github.com>
2021-10-12 17:16:36 -0500

    tailor made images

--- tailors/admin.py
+++ tailors/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from .models import Tailor
+from adminsortable2.admin import SortableAdminMixin
+
 
 # Register your models here.
 @admin.register(Tailor)
-class TailorAdmin(admin.ModelAdmin):
-    list_display = ('title', 'active')
-    search_fields = ('title',)
-
-
+class TailorAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ("title", "active")
+    search_fields = ("title",)
--- tailors/models.py
+++ tailors/models.py
@@ -8,33 +8,35 @@ from imagekit.processors import ResizeToFill
 from autoslug import AutoSlugField
 import os
 
+
+def path_and_rename(instance, filename):
+    upload_to = "images/tailor/"
+    ext = filename.split(".")[-1]
+    filename = "{}.{}".format(instance.title, ext)
+    return os.path.join(upload_to, filename)
+
+
 class Tailor(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField(default='')
-
-    image = models.FileField(upload_to='images/tailors/', blank=True, null=True)
-
-    thumbnail = ImageSpecField(
-        source='image',
-        processors=[ResizeToFill(545, 276)],
-        format='JPEG',
-        options={'quality': 98},
-    )
-
-    original = ImageSpecField(
-        source='image',
-        processors=[ResizeToFill(1500, 800)],
-        format='JPEG',
-        options={'quality': 98},
+    content = models.TextField(default="")
+    image = ProcessedImageField(
+        blank=True,
+        null=True,
+        upload_to=path_and_rename,
+        processors=[ResizeToFill(490, 250)],
+        format="JPEG",
+        options={"quality": 100},
     )
 
     active = models.BooleanField(default=False)
+    order = models.PositiveIntegerField(default=0, blank=False, null=False)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'tailor'
+        db_table = "tailor"
+        ordering = ["order"]
 
     def __str__(self):
         return self.title
