9f29b73d7ed92f99d34677507e5630ba0ad23694
Aldair Huamani Cusi <32407895+Baku452@users.noreply.github.com>
2021-11-17 12:17:34 -0500

    remove api key

--- valenciaApi/settings.py
+++ valenciaApi/settings.py
@@ -147,10 +147,6 @@ EMAIL_PORT = "587"
 EMAIL_HOST_USER = "seo@valenciatravelcusco.com"
 EMAIL_HOST_PASSWORD = os.getenv("DJANGO_EMAIL_PASSWORD")
 EMAIL_USE_TLS = True
-# Twilio SendGrid
-# EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
-# SENDGRID_API_KEY = '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'
-# SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
